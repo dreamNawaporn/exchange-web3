1850022f7399c7da6a98ab538c4754fbfb40611c
dreamNawaporn <644259029@webmail.npru.ac.th>
2023-11-27 17:35:58 +0700

    ..

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,214 @@
-import Image from 'next/image'
+"use client";
+import React, { useState, useEffect } from "react";
 
-export default function Home() {
+
+import { initializeConnector } from "@web3-react/core";
+import { MetaMask } from "@web3-react/metamask";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+
+
+import {
+  Container,
+  Card,
+  CardContent,
+  TextField,
+  Divider,
+} from "@mui/material";
+
+
+// Knowledge about Ether.js https://docs.ethers.org/v6/getting-started/
+import { ethers } from "ethers";
+import { formatEther, parseUnits } from "ethers";
+import abi from "./abi.json";
+
+
+const [metaMask, hooks] = initializeConnector(
+  actions => new MetaMask({ actions })
+);
+const { useChainId, useAccounts, useIsActivating, useIsActive, useProvider } =
+  hooks;
+const contractChain = 11155111;
+
+
+const contractAddress = "0xafeC49728d68Fd109B24dC417225aBe8fdEdDFFD";
+//const contractAddress = "0xb3B9eD674453E88054879526Cccb18bD2b6Ce1a9";
+
+
+export default function Page() {
+  const chainId = useChainId();
+  const accounts = useAccounts();
+  const isActive = useIsActive();
+
+
+  const provider = useProvider();
+  const [error, setError] = useState(undefined);
+
+
+  const [balance, setBalance] = useState("");
+  useEffect(() => {
+    const fetchBalance = async () => {
+      const signer = provider.getSigner();
+      const smartContract = new ethers.Contract(contractAddress, abi, signer);
+      const myBalance = await smartContract.balanceOf(accounts[0]);
+      console.log(formatEther(myBalance));
+      setBalance(formatEther(myBalance));
+    };
+
+
+    if (isActive) {
+      fetchBalance();
+    }
+  }, [isActive]);
+
+
+  const [neeValue, setNeeValue] = useState(0);
+
+
+  const handleSetNeeValue = event => {
+    setNeeValue(event.target.value);
+  };
+
+
+  const handleBuy = async () => {
+    try {
+      if (neeValue <= 0) {
+        return;
+      }
+
+
+      const signer = provider.getSigner();
+      const smartContract = new ethers.Contract(contractAddress, abi, signer);
+      const buyValue = parseUnits(neeValue.toString(), "ether");
+      const tx = await smartContract.buy({
+        value: buyValue.toString(),
+      });
+      console.log("Transaction hash:", tx.hash);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+  useEffect(() => {
+    void metaMask.connectEagerly().catch(() => {
+      console.debug("Failed to connect to metamask");
+    });
+  }, []);
+
+
+  const handleConnect = () => {
+    metaMask.activate(contractChain);
+  };
+
+
+  const handleDisconnect = () => {
+    metaMask.resetState();
+  };
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="#B768FE"
+            aria-label="menu"
+            sx={{ mr: 2 }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Crypto exchange
+          </Typography>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {isActive ? (
+            <Stack direction="row" spacing={1} >
+              <Chip label={accounts[0]} variant="outlined" />
+              <Button class="w3-btn w3-round-xxlarge" color="inherit" onClick={handleDisconnect}>
+                Disconnect
+              </Button>
+            </Stack>
+          ) : (
+            <Button class="w3-btn w3-round-xxlarge" color="inherit" onClick={handleConnect}>
+              Connect
+            </Button>
+          )}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        </Toolbar>
+      </AppBar>
+      {isActive && (
+        <Container maxWidth="sm" sx={{ mt: 2 }}>
+          <Card>
+            <CardContent>
+              <Stack spacing={2}>
+                <Typography
+                  sx={{ fontSize: 20 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  My wallet balance
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  label="Address"
+                  value={accounts[0]}
+                  variant="outlined"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Dre Balance"
+                  value={balance}
+                  variant="outlined"
+                />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+
+                <Divider />
+                <Typography
+                  sx={{ fontSize: 20 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  Buy Dre Token
+                </Typography>
+
+
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Enter amount of Ether you want to buy Dre Token"
+                  defaultValue=""
+                  type="number"
+                  onChange={handleSetNeeValue}
+                />
+
+
+                <Button variant="contained" onClick={handleBuy}>
+                  Buy Dre Token
+                </Button>
+              </Stack>
+            </CardContent>
+          </Card>
+        </Container>
+      )}
+    </Box>
+  );
 }
+
+
+// w3school - React Ternary operator https://www.w3schools.com/react/react_es6_ternary.asp
